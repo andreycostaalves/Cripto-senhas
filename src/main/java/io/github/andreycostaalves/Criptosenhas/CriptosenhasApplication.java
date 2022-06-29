59fcc75466756584dec5ca1f6fd7152d96660809
Andrey Alves <Andreyalvesgit@gmail.com>
2022-06-29 13:50:00 -0300

    Criptografia: Comparando senhas com banco de dados.

--- src/main/java/io/github/andreycostaalves/Criptosenhas/CriptosenhasApplication.java
+++ src/main/java/io/github/andreycostaalves/Criptosenhas/CriptosenhasApplication.java
@@ -3,12 +3,22 @@ package io.github.andreycostaalves.Criptosenhas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptosenhasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CriptosenhasApplication.class, args);
+	}
+
+	/**Criptografando senhas usando BCrypt Encoder*/
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
+		return encoder;
 	}
 
 }
